46112e920c999cb66463eae3c487e70b9c756082
bobby <schroeder.brian@gmx.de>
2025-02-28 08:42:10 +0100

    admin test

---
+++ js/config.js
@@ -0,0 +1,14 @@
+/**
+ * Konfigurationsdatei für die Dyslexie-Studie
+ */
+
+const CONFIG = {
+    // Admin-Passwort für den Zugriff auf die Studiendaten
+    ADMIN_PASSWORD: "12345",
+    
+    // Datenbank-Einstellungen
+    DATABASE: {
+        name: "dyslexie_studie",
+        version: 1
+    }
+}; 
